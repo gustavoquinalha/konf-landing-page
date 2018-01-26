03e8d4d80580e732abf3609160e9efd1d3d2d696
gustavoquinalha <gustavo_quinalha@hotmail.com>
2018-01-26 15:54:55 -0200

    typed.js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  new Typed('#typed6', {
+    strings: ['$ npm install -g konf\n`installing Konf...` ^1000\n`$ Konf init`'],
+    typeSpeed: 40,
+    backSpeed: 0,
+    loop: true
+  });
+
+});
