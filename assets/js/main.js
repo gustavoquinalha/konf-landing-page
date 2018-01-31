a9bfdf946b4ac9fa436259333464f1f1a243961b
gustavoquinalha <gustavo_quinalha@hotmail.com>
2018-01-31 18:09:09 -0200

    desenho

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  new Typed('#typed6', {
-    strings: ['$ npm install -g konf\n`installing Konf...` ^1000\n`$ Konf init`'],
+  new Typed('#code', {
+    strings: ['$ npm install -g konf\n`installing Konf...\n`$ Konf init'],
     typeSpeed: 40,
-    backSpeed: 0,
     loop: true
   });
 
