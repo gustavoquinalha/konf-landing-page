73d1a15f6f16f631bdd087d801214057e1947f3a
gustavoquinalha <gustavo_quinalha@hotmail.com>
2018-02-07 18:14:26 -0200

    issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  new Typed('#code', {
-    strings: ['$ npm install -g konf\n`installing Konf...\n`$ Konf init'],
+  new Typed('#codeHeader', {
+    strings: ['$ npm install -g @konf/konf\n`Installing Konf...` ^1000\n$ konf new^1000\n$ konf server^1000\n$ konf build\n`Building konf...` ^1000'],
     typeSpeed: 40,
     loop: true
   });
+
+  const htmlClass = document.documentElement;
+  const btnMenu = document.getElementById("btn-menu");
+  const contentMenu = document.getElementById("menu-content");
+  const iconMenu = document.getElementById("icon-menu");
+
+  btnMenu.addEventListener('click', function toggleMenu () {
+    contentMenu.classList.toggle('open');
+    htmlClass.classList.toggle('menu-open');
+    iconMenu.classList.toggle("fa-bars");
+    iconMenu.classList.toggle("fa-times");
+  });
+
 
 });
